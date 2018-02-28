4198e4aae7b6e5b94bada8555882447bec349901
Pavel Lautsevich <zigzag.mcquack@gmail.com>
2018-02-28 20:46:38 +0300

    Change Google Mussic Player version

--- googlemusicplayer.sh
+++ googlemusicplayer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /tmp
-wget https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases/download/v4.2.0/google-play-music-desktop-player_4.2.0_amd64.deb
+wget https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases/download/v4.5.0/google-play-music-desktop-player_4.5.0_amd64.deb
 sudo dpkg -i google-play-music-desktop-player_*_amd64.deb
 sudo rm -rf google-play-music-desktop-player_*_amd64.deb
