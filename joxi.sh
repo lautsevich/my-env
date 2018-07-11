0eef07d3eedaf5991ae4e78082ac310c9b4f8d9e
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-07-11 12:33:37 +0300

    Add TODO to Joxi install

--- joxi.sh
+++ joxi.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cd /tmp
 wget http://dl.joxi.ru/linux/joxi-amd64.deb
 sudo dpkg -i joxi-amd64.deb
 sudo rm -rf joxi-amd64.deb
 sudo apt-get -f install -y
+
+# TODO: Need fix dependecies fail
