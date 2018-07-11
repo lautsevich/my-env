8f882c81740da3f5c8d6a376929962c82aa8be6e
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-07-11 13:41:59 +0300

    Fix MySQL install

--- mysql.sh
+++ mysql.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password lovata'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password lovata'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password lovata'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password lovata'
 
 sudo apt-get update
 sudo apt-get install -y mysql-server
