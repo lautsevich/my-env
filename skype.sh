1ade882572e9bd9866292240104e4846969b21e3
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-07-11 13:17:33 +0300

    Fix Skype install

--- skype.sh
+++ skype.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-cd /tmp
 curl https://repo.skype.com/data/SKYPE-GPG-KEY | sudo apt-key add -
 echo "deb https://repo.skype.com/deb stable main" | sudo tee /etc/apt/sources.list.d/skypeforlinux.list
 sudo apt-get update
 sudo apt-get install -y skypeforlinux
-
