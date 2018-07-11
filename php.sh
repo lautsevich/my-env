9a7cc651add3dda1011d5ba1c874f84b6e5adb74
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-07-11 14:41:50 +0300

    Add PHP install

---
+++ php.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y php php-common
+sudo apt-get install -y php-json php-phpdbg php-dev php-xml
+sudo apt-get install -y php-cli
+sudo apt-get install -y libapache2-mod-php
+sudo apt-get install -y php-mysql
+sudo apt-get install -y php-gd
+sudo apt-get install -y php-curl
+sudo apt-get install -y php-mbstring
+sudo apt-get install -y php-zip
+sudo apt-get install -y php-pear
