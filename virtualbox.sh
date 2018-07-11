31b7bea88fcb9565316f1c6d5e7dedf6d0c1fad9
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-07-11 13:41:24 +0300

    Fix Virtualbox install

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian xenial contrib" >> /etc/apt/sources.list'
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install -y virtualbox-5.2
+sudo apt-get install -y virtualbox
