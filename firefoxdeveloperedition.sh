85e1b4ed9687773c7dc58efef5d8a6ec88c2bc4b
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-07-11 12:09:28 +0300

    Remove Firefox Developer Edition install

--- firefoxdeveloperedition.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
-sudo apt-get update
-sudo apt-get install ubuntu-make
-umake web firefox-dev
-sudo ln -s ~/.local/share/umake/web/firefox-dev/firefox /usr/bin/firefox-dev
