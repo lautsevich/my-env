2a4882ec55674573e95480be6313330546f0ac65
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-07-11 12:05:49 +0300

    Fix Avocode install

--- avocode.sh
+++ avocode.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cd /tmp
 curl https://linux.avocode.com/avocode_pub.gpg | sudo apt-key add -
+sudo su
 echo deb http://linux.avocode.com/apt all main >> /etc/apt/sources.list
+exit
 sudo apt-get update
 sudo apt-get install -y avocode
