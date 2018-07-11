f376ccbcae0f10919bbf42a6f9229bd53e99b635
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-07-11 13:42:44 +0300

    Fix Stride autostart in install script

--- stride.sh
+++ stride.sh
@@ -1,24 +1,26 @@
-#!/bin/bash
+!/bin/bash
 
-cd /tmp
 sudo sh -c 'echo "deb https://packages.atlassian.com/debian/stride-apt-client $(lsb_release -c -s) main" > /etc/apt/sources.list.d/atlassian-stride.list'
 wget -O - https://packages.atlassian.com/api/gpg/key/public | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y stride
 
 APP_NAME=Stride
+AUTOSTART_FILE=$APP_NAME.desktop
 APP_EXEC="/usr/share/applications/stride %U"
-AUTOSTART_CONFIG=~/.config/autostart/$APP_NAME.desktop
+AUTOSTART_PATH=~/.config/autostart/
 
-sudo touch $AUTOSTART_CONFIG
+touch $AUTOSTART_FILE
 
-sudo echo "[Desktop Entry]" >> $AUTOSTART_CONFIG
-sudo echo "Type=Application" >> $AUTOSTART_CONFIG
-sudo echo "Exec=$APP_EXEC" >> $AUTOSTART_CONFIG
-sudo echo "Hidden=false" >> $AUTOSTART_CONFIG
-sudo echo "NoDisplay=false" >> $AUTOSTART_CONFIG
-sudo echo "X-GNOME-Autostart-enabled=true" >> $AUTOSTART_CONFIG
-sudo echo "Name[en_US]=$APP_NAME" >> $AUTOSTART_CONFIG
-sudo echo "Name=$APP_NAME" >> $AUTOSTART_CONFIG
-sudo echo "Comment[en_US]=" >> $AUTOSTART_CONFIG
-sudo echo "Comment=" >> $AUTOSTART_CONFIG
+echo "[Desktop Entry]
+Type=Application
+Exec=$APP_EXEC
+Hidden=false
+NoDisplay=false
+X-GNOME-Autostart-enabled=true
+Name[en_US]=$APP_NAME
+Name=$APP_NAME
+Comment[en_US]=
+Comment=" >> $AUTOSTART_FILE
+
+sudo mv $AUTOSTART_FILE $AUTOSTART_PATH
