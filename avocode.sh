2532947a42d274d4281f6731d14e25c4cf9c2ce4
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-07-11 15:00:45 +0300

    Revert Avocode to previous install state

--- avocode.sh
+++ avocode.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 curl https://linux.avocode.com/avocode_pub.gpg | sudo apt-key add -
-sudo su
-echo deb http://linux.avocode.com/apt all main >> /etc/apt/sources.list
-exit
+sudo echo deb http://linux.avocode.com/apt all main >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -y avocode
