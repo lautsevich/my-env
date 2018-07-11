febd4e63290c08c4baddb864999c2dd06afc34e2
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-07-11 12:17:16 +0300

    Add TODO to GitKraken install

--- gitkraken.sh
+++ gitkraken.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cd /tmp
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
 sudo rm -rf gitkraken-amd64.deb
+
+# TODO: Need fix python dependecy fail
