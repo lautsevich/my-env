de3efae18aff4d3a91dcbf86a52369cd4ff0dcf5
Pavel Lovtsevich <p.lovtsevich@lovata.com>
2018-07-11 13:07:27 +0300

    Fix Opera install

--- opera.sh
+++ opera.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cd /tmp
 wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
 sudo add-apt-repository 'deb https://deb.opera.com/opera-stable/ stable non-free'
-sudo apt-get update
 sudo apt-get install -y opera-stable
+
+# TODO: Add deb-conf data for silent install
+# TODO: Fix Opera multiole times configuration in sources
