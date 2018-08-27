a289d768d3ac5f2ffd9c986635c070aaa1a13610
Pavel Lautsevich <p.lovtsevich@lovata.com>
2018-08-27 13:24:46 +0300

    Add global install step to Apache2 script

--- apache2.sh
+++ apache2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update
+INSTALL_BULK=$1
+
+if [[ "$INSTALL_BULK" = false ]]; then
+    sudo apt-get update
+fi
+
 sudo apt-get install -y apache2
 
 # TODO: Add apache.conf user & group replacement
--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@
 INSTALL_BULK=true
 
 bash acestream.sh $INSTALL_BULK
+bash apache2.sh $INSTALL_BULK
