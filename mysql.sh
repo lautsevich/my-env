69e4b6c092553f2ee88b3e7e26e0d835cbb18f43
Pavel Lautsevich <p.lovtsevich@lovata.com>
2018-08-27 14:45:20 +0300

    Add global install step to MySQL script

--- mysql.sh
+++ mysql.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+INSTALL_BULK=$1
+
+if [[ "$INSTALL_BULK" = false ]]; then
+    sudo apt-get update
+fi
+
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password lovata'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password lovata'
 
-sudo apt-get update
 sudo apt-get install -y mysql-server
 sudo apt-get install -y mysql-client
