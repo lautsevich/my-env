8838a0bc3b4d4e4965a63cf5693ce8dd90a9737f
Pavel Lautsevich <p.lovtsevich@lovata.com>
2018-08-28 13:23:44 +0300

    Add global install step to PhpMyAdmin script

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-APP_PASS="password"
-ROOT_PASS="password"
-APP_DB_PASS="password"
+INSTALL_BULK=$1
+APP_PASS="lovata"
+ROOT_PASS="lovata"
+APP_DB_PASS="lovata"
+
+if [[ "$INSTALL_BULK" = false ]]; then
+    sudo apt-get update
+fi
 
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $APP_PASS"
@@ -10,7 +15,8 @@ sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $APP_DB_PASS"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 
-sudo apt-get update
 sudo apt-get install -y phpmyadmin php-mbstring php-gettext
 sudo phpenmod mbstring
 sudo systemctl restart apache2
+
+# TODO: Move password to config
