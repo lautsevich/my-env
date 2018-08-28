3702d8ea3deecc3feb7fcf12d90662cc46d28a3c
Pavel Lautsevich <p.lovtsevich@lovata.com>
2018-08-28 13:25:00 +0300

    Remove unnecessary step from Teamviewer script

--- teamviewer.sh
+++ teamviewer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd /tmp
 sudo dpkg --add-architecture i386
 sudo apt-get update
 sudo apt-get install -y libdbus-1-3:i386 libasound2:i386 libexpat1:i386 libfontconfig1:i386 libfreetype6:i386 libjpeg62:i386 libpng12-0:i386 libsm6:i386 libxdamage1:i386 libxext6:i386 libxfixes3:i386 libxinerama1:i386 libxrandr2:i386 libxrender1:i386 libxtst6:i386 zlib1g:i386 libc6:i386
