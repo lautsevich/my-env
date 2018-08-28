f3eff10400e0e381741da2faa231430b2b942a33
Pavel Lautsevich <p.lovtsevich@lovata.com>
2018-08-28 13:25:13 +0300

    Update global install script

--- install.sh
+++ install.sh
@@ -1,6 +1,53 @@
 #!/bin/bash
 
-INSTALL_BULK=true
+# INSTALL_BULK=true
 
-bash acestream.sh $INSTALL_BULK
+# UTILS
+bash curl.sh $INSTALL_BULK
+bash keepassxc.sh $INSTALL_BULK
+
+# DEVELOPMENT
+bash git.sh $INSTALL_BULK
+bash gitlfs.sh $INSTALL_BULK
+bash gitkraken.sh $INSTALL_BULK
 bash apache2.sh $INSTALL_BULK
+bash mysql.sh $INSTALL_BULK
+bash php.sh $INSTALL_BULK
+bash composer.sh $INSTALL_BULK
+bash phpmyadmin.sh $INSTALL_BULK
+bash ruby.sh $INSTALL_BULK
+bash bundler.sh $INSTALL_BULK
+bash nodejs.sh $INSTALL_BULK
+bash code.sh $INSTALL_BULK
+bash webstorm.sh $INSTALL_BULK
+# bash avocode.sh $INSTALL_BULK
+
+# INTERNET
+bash chromium.sh $INSTALL_BULK
+bash opera.sh $INSTALL_BULK
+bash yandexbrowser.sh $INSTALL_BULK
+bash skype.sh $INSTALL_BULK
+bash telegram.sh $INSTALL_BULK
+bash slack.sh $INSTALL_BULK
+bash stride.sh $INSTALL_BULK
+# bash viber.sh $INSTALL_BULK
+
+# MEDIA
+bash vlc.sh $INSTALL_BULK
+bash acestream.sh $INSTALL_BULK
+bash sopcast.sh $INSTALL_BULK
+bash googlemusicmanager.sh $INSTALL_BULK
+bash googlemusicplayer.sh $INSTALL_BULK
+# bash joxi.sh $INSTALL_BULK
+bash steam.sh $INSTALL_BULK
+bash simplescreenrecorder.sh $INSTALL_BULK
+
+# OS
+bash gnome-tweaks.sh $INSTALL_BULK
+bash krusader.sh $INSTALL_BULK
+bash mc.sh $INSTALL_BULK
+bash teamviewer.sh $INSTALL_BULK
+bash virtualbox.sh $INSTALL_BULK
+bash vagrant.sh $INSTALL_BULK
+bash vine.sh $INSTALL_BULK
+bash playonlinux.sh $INSTALL_BULK
