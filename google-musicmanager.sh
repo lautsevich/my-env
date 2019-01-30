5181d3c9c1f45c43b187a3b167ef49a681dc22ed
Pavel Lautsevich <p.lovtsevich@lovata.com>
2019-01-30 19:58:50 +0300

    Rename Google Music Manager setup file

--- google-musicmanager.sh
+++ google-musicmanager.sh
@@ -4,7 +4,3 @@ sudo sh -c 'echo "deb http://dl.google.com/linux/musicmanager/deb/ stable main" 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install -y google-musicmanager-beta
-
-#wget https://dl.google.com/linux/direct/google-musicmanager-beta_current_amd64.deb
-#sudo dpkg -i google-musicmanager-beta_current_amd64.deb
-#sudo rm -rf google-musicmanager-beta_current_amd64.deb
