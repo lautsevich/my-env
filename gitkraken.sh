a58c6bf54c96d88492c78fc6cc951c23599fee6f
Pavel Lautsevich <p.lovtsevich@lovata.com>
2019-01-30 20:21:23 +0300

    Fix GitKraken broken install

--- gitkraken.sh
+++ gitkraken.sh
@@ -3,5 +3,4 @@
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
 sudo dpkg -i gitkraken-amd64.deb
 sudo rm -rf gitkraken-amd64.deb
-
-# TODO: Need fix python dependecy fail
+sudo apt --fix-broken install -y
