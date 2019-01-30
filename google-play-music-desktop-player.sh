ac0a33d5a2a9db1f3306d2adae3121db57f0dbbc
Pavel Lautsevich <p.lovtsevich@lovata.com>
2019-01-30 20:00:51 +0300

    Rename Google Music Desktop Player setup file

--- google-play-music-desktop-player.sh
+++ google-play-music-desktop-player.sh
@@ -3,3 +3,4 @@
 wget https://github.com/MarshallOfSound/Google-Play-Music-Desktop-Player-UNOFFICIAL-/releases/download/v4.6.1/google-play-music-desktop-player_4.6.1_amd64.deb
 sudo dpkg -i google-play-music-desktop-player_*_amd64.deb
 sudo rm -rf google-play-music-desktop-player_*_amd64.deb
+sudo apt --fix-broken install -y
