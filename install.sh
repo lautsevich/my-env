0e121649fdc494b6d73f6f0e34b0b2e2592615d3
Pavel Lautsevich <p.lovtsevich@lovata.com>
2019-01-30 20:40:57 +0300

    Update main install following previous changes

--- install.sh
+++ install.sh
@@ -2,13 +2,17 @@
 
 # INSTALL_BULK=true
 
+# Remove unnecesary apps
+bash clean-ubuntu.sh
+
 # UTILS
 bash curl.sh $INSTALL_BULK
 bash keepassxc.sh $INSTALL_BULK
+bash gufw.sh $INSTALL_BULK
 
 # DEVELOPMENT
 bash git.sh $INSTALL_BULK
-bash gitlfs.sh $INSTALL_BULK
+bash git-lfs.sh $INSTALL_BULK
 bash gitkraken.sh $INSTALL_BULK
 bash apache2.sh $INSTALL_BULK
 bash mysql.sh $INSTALL_BULK
@@ -18,9 +22,15 @@ bash phpmyadmin.sh $INSTALL_BULK
 bash ruby.sh $INSTALL_BULK
 bash bundler.sh $INSTALL_BULK
 bash nodejs.sh $INSTALL_BULK
+bash build-essential.sh $INSTALL_BULK
+bash ansible.sh $INSTALL_BULK
+bash terraform.sh $INSTALL_BULK
+bash docker-ce.sh $INSTALL_BULK
 bash code.sh $INSTALL_BULK
 bash webstorm.sh $INSTALL_BULK
-# bash avocode.sh $INSTALL_BULK
+bash avocode.sh $INSTALL_BULK
+bash figma.sh $INSTALL_BULK
+bash avocode.sh $INSTALL_BULK
 
 # INTERNET
 bash chromium.sh $INSTALL_BULK
@@ -29,16 +39,15 @@ bash yandexbrowser.sh $INSTALL_BULK
 bash skype.sh $INSTALL_BULK
 bash telegram.sh $INSTALL_BULK
 bash slack.sh $INSTALL_BULK
-bash stride.sh $INSTALL_BULK
 bash viber.sh $INSTALL_BULK
 
 # MEDIA
 bash vlc.sh $INSTALL_BULK
 bash acestream.sh $INSTALL_BULK
 bash sopcast.sh $INSTALL_BULK
-bash googlemusicmanager.sh $INSTALL_BULK
-bash googlemusicplayer.sh $INSTALL_BULK
-# bash joxi.sh $INSTALL_BULK
+bash google-musicmanager.sh $INSTALL_BULK
+bash google-play-music-desktop-player.sh $INSTALL_BULK
+bash flameshot.sh $INSTALL_BULK
 bash steam.sh $INSTALL_BULK
 bash simplescreenrecorder.sh $INSTALL_BULK
 
@@ -49,5 +58,10 @@ bash mc.sh $INSTALL_BULK
 bash teamviewer.sh $INSTALL_BULK
 bash virtualbox.sh $INSTALL_BULK
 bash vagrant.sh $INSTALL_BULK
-bash vine.sh $INSTALL_BULK
+bash wine.sh $INSTALL_BULK
 bash playonlinux.sh $INSTALL_BULK
+bash gnome-weather.sh $INSTALL_BULK
+bash ubuntu-restricted-extras.sh $INSTALL_BULK
+bash libavcodec-extra.sh $INSTALL_BULK
+bash stacer.sh $INSTALL_BULK
+bash gnome-shell-extensions.sh $INSTALL_BULK
