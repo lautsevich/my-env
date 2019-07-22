90b2f77235fd92c0cad4e63865b5aff58320ec05
Pavel Lautsevich <p.lovtsevich@lovata.com>
2019-07-22 21:05:07 +0300

    Add silent install to Docker CE dependencies

--- docker-ce.sh
+++ docker-ce.sh
@@ -6,7 +6,7 @@ if [[ "$INSTALL_BULK" = false ]]; then
     sudo apt-get update
 fi
 
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt-get update
