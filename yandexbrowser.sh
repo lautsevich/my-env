480d84d4933ba2d7dfc6f55224ba0506d8195958
Pavel Lautsevich <p.lovtsevich@lovata.com>
2020-04-26 21:50:35 +0300

    Add Yandex Browser

--- yandexbrowser.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo sh -c 'echo "deb [arch=amd64] http://repo.yandex.ru/yandex-browser/deb beta main" > /etc/apt/sources.list.d/yandex-browser-beta.list'
-wget -q -O - https://repo.yandex.ru/yandex-browser/YANDEX-BROWSER-KEY.GPG | sudo apt-key add -
-sudo apt-get update
-sudo apt-get install -y yandex-browser-beta
