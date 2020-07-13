4edb4ac0a2c408ec2f94a05e4b55c5e55f9a22b1
Pavel Lautsevich <p.lovtsevich@lovata.com>
2020-07-13 12:39:05 +0300

    Add apt upgrade and autoremove to install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 sudo apt update
+sudo apt upgrade
 sudo apt install -y git
 sudo apt install -y snapd
 sudo apt install -y ansible
 sudo apt install -y make
+sudo apt autoremove
 
 echo "Run make provision"
